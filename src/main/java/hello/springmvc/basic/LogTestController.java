cd709a316767bdf2a4a784a5b91f8ed51dec4fdb
Byeongsu Kim <48823001+ingal9394@users.noreply.github.com>
2022-03-29 17:35:16 +0900

    logging

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,19 +1,24 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // 원래 controller 로 어노테이션하면 viewresolver 가 돌면서 viewname으로반환되는데 restcontroller는 body에  반환값을 팍 넣음
+@Slf4j
+@RestController // 원래 controller 로 어노테이션하면 반환값이 string 이면 viewresolver 가 돌면서 viewname으로반환되어 뷰가 렌더링됨
+// restcontroller는 http body에  반환값을 넣음 = 실행결과로 홈페이지에 ok표시됨
 public class LogTestController {
-    private final Logger log = LoggerFactory.getLogger(getClass());//slf4j 로 추가해야함
+    //private final Logger log = LoggerFactory.getLogger(getClass());//slf4j 로 추가해야함(추가하면 어노테이션에 자동으로 포함되어있는거 lombok)
 
     @RequestMapping("/log-test")
     public String logTest(){
         String name = "Spring";
 
         System.out.println("name = " + name);
+        //log.trace(" trace log ="+name); 이렇게 써놓고 레벨을 debug로 두고 로깅하면 trace는 안나오지만 
+        //                                java문법상 출력은안되도  name에 대입 해서 결과값은 가지고있게되기에 리소스를먹어서 이렇게 쓰면 안됨 밑에것 처럼 써야함 {} 함수처럼
         log.trace(" trace log = {}",name); //레벨임 application.properties 에서 logging 설정시  어디까지 보여질것인가  trace(개발)/debug(개발)/info(운영) 보통 이렇게설정
         log.debug(" debug log = {}",name);
         log.info(" info log = {}",name);
---
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,36 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap, //하나의 키에 여러값을 받을떄 사용함(같은키에 배열형식으로 들어감)
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "MyCookie", required = false) String cookie) {
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+        return "ok";
+    }
+
+}
